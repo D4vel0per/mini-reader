f3d1322bdf0e6e897fd75dcddd906760b04719c8
D4veloper <omardavidmendezmontiel@gmail.com>
2026-08-10 19:53:57 -0400

    added custom plugin to support Java 8+ and added functionality to Reader component

--- src/components/Reader.tsx
+++ src/components/Reader.tsx
@@ -1,28 +1,128 @@
 import type {
+    Decoration,
+    DecorationActivatedEvent,
+    DecorationGroup,
     File,
-    SelectionAction
+    SelectionAction,
+    SelectionActionEvent
 } from 'react-native-readium';
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Text, View } from 'react-native';
 import { ReadiumView } from "react-native-readium";
 
 interface ReaderProps {
     uri: string;
+    userTint: string;
+    username: string;
 }
 
-const actions: SelectionAction[] = [
-    { id: "comment", label: 'Comment' }
-]
+interface CommentProps {
+    text: string
+    username: string
+}
 
-export default function Reader({uri}: ReaderProps) {
+function Comment ({ text, username }: CommentProps) {
+    return (
+        <View>
+            <Text>{username} - { text }</Text>
+        </View>
+    )
+}
+
+function fromDecorations(groups: DecorationGroup[], key: string) {
+    const data = groups.filter(e => e.name === key)
+
+    return data.length ? data[0].decorations : undefined
+}
+
+interface DecorationOverrideExtra extends Record<string, string> {
+    text: string;
+    username: string;
+}
+
+interface DecorationOverride extends Decoration {
+    extras: DecorationOverrideExtra
+}
+
+export default function Reader({ uri, userTint, username }: ReaderProps) {
     const [ file] = useState<File>({
         url: uri,
     });
+    const [ decorations, setDecorations ] = useState<DecorationGroup[]>([
+        { name: "comments", decorations: [] }
+    ]);
+
+    const [ actions, setActions ] = useState<SelectionAction[]>([
+        { id: "comment", label: 'Comment' }
+    ])
+
+    const [ showComments, setShowComments ] = useState(false)
+
+    useEffect(() => {
+        const comments = fromDecorations(decorations, "comments")
+
+        if (!comments) return;
+
+        setActions([
+            { id: "comment", label: 'Comment' },
+            { id: "lookup", label: `Look up ${comments.length} comments` }
+        ])
+    }, [ decorations ])
+
+    const onSelection = (e: SelectionActionEvent) => {
+        console.log("Action id: ", e.actionId)
+
+        if (e.actionId === "lookup") {
+            setShowComments(true)
+            return
+        }
+
+        const commentDecoration: DecorationOverride = {
+            id: `comment-${Date.now()}`,
+            locator: e.locator,
+            style: {
+                type: "highlight",
+                tint: userTint
+            },
+            extras: {
+                text: e.selectedText,
+                username: "admin"
+            }
+        }
+
+        setDecorations(prev => prev.map(
+            dec => dec.name !== "comments" ? dec
+            : { ...dec, decorations: [ ...dec.decorations, commentDecoration ] }
+        ))
+    }
+
+    const onCommentPressed = ({ decoration }: DecorationActivatedEvent) => {
+        console.log('Comment pressed')
+        console.log("Selected text: ", decoration.extras?.text)
+        console.log("Username: ", decoration.extras?.username)
+    }
 
     return (
+        <>
         <ReadiumView 
         file={file} 
         preferences={{}} 
+        selectionActions={actions}
+        decorations={decorations}
+        onSelectionAction={onSelection}
+        onDecorationActivated={onCommentPressed}
         />
+        {
+            showComments &&
+            <View style={{position: 'absolute', flexDirection: 'column'}}>
+                { fromDecorations(decorations, "comments")?.map(e => {
+                    const comment = e as DecorationOverride
+                    return <Comment text={comment.extras.text} username={comment.extras.username}/>
+                }) }
+            </View>
+        }
+        </>
+        
     )
 }
---
+++ withCustomAppBuildGradle.js
@@ -0,0 +1,23 @@
+const { withAppBuildGradle } = require('expo/config-plugins');
+
+function insert (oldText, pattern, newText) {
+    const pivot = oldText.indexOf(pattern) + pattern.length + 1
+    return oldText.slice(0, pivot) + newText + oldText.slice(pivot)
+}
+
+module.exports = function withCustomAppBuildGradle(config) {
+ return withAppBuildGradle(config, async (config) => {
+   const androidPattern = '\nandroid {\n';
+   const dependenciesPattern = '\ndependencies {\n';
+   const androidText = '   compileOptions {\n        coreLibraryDesugaringEnabled true\n    }\n\n '
+   const dependenciesText = '   coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")\n\n '
+   let { contents } = config.modResults
+
+   contents = insert(contents, androidPattern, androidText)
+   contents = insert(contents, dependenciesPattern, dependenciesText)
+
+   config.modResults.contents = contents
+
+   return config;
+ });
+};
